da16cb9a8da7e8859b36ea9376f1bd80f3a936f2
Dave Richter <rialdave32@googlemail.com>
2020-05-02 18:12:36 +0200

    [TASK] Sets development version again

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ ScriptName = "VIPTools"
 Website = "https://twitch.tv/rialDave/"
 Description = "Adds new features for Twitchs VIP functionality"
 Creator = "rialDave"
-Version = "0.5.0"
+Version = "0.5.0-dev"
 
 #---------------------------
 #   Global Variables
